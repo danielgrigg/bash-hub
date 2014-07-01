5c83fdcb62a48d9148338ea763e8d6a917210466
Daniel Grigg <mail@danielgrigg.com>
2014-07-01 18:54:06 +1000

    Add the mkbash script and a bootstrap bash script

--- bin/boot.bash
+++ bin/boot.bash
@@ -1,10 +1,16 @@
 #!/usr/bin/env bash
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
+set -o nounset
+set -o errexit
+
+#DEBUG=1
+
+SOURCE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 NORMAL=$(tput sgr0)
 GREEN=$(tput setaf 2; tput bold)
 YELLOW=$(tput setaf 3)
 RED=$(tput setaf 1)
+
 
 function red() {
   echo -e "$RED$*$NORMAL"
@@ -17,4 +23,12 @@ function green() {
 function yellow() {
   echo -e "$YELLOW$*$NORMAL"
 }
+
+function log {
+  local prefix="[$(date +%Y/%m/%d\ %H:%M:%S)]: "
+  echo "${prefix} $@" >&2
+}
+# eg, log "WARN" "Warning here"
+
+function debug() { ((DEBUG)) && echo ">>> $*"; }
 
